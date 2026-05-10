e59ea0daed263ad92fbfbb8c3077370f07286513
duvindu-embuldeniya <duvindu.personal@gmail.com>
2026-05-10 13:43:31 +0530

    add: backend health + probes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,9 @@ from . views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('health/', health, name = 'health'),
+    path('startup/', startup, name= 'startup'),
+    path('ready/', readiness, name = 'ready'),
+    path('live/', liveness, name = 'live'),
     path('register/', register, name = 'register')
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,9 +6,46 @@ from . serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.http import HttpResponse
+from django.db import connections
+from django.db.utils import OperationalError
+
 
 def home(request):
     return HttpResponse("<h1>Home Page</h1>")
+
+
+def health(request):
+    for db_name in connections:
+        try:
+            connections[db_name].cursor()
+        except OperationalError:
+            return HttpResponse("status: db down", status=503)
+
+    return HttpResponse("status: ok", status=200)
+
+def startup(request):
+    return HttpResponse("started", status=200)
+
+
+def readiness(request):
+    for db_name in connections:
+        try:
+            connections[db_name].cursor()
+        except OperationalError:
+            return HttpResponse("db down", status=503)
+
+    return HttpResponse("ready", status=200)
+
+
+def liveness(request):
+    for db_name in connections:
+        try:
+            connections[db_name].cursor()
+        except OperationalError:
+            return HttpResponse("db down", status=503)
+
+    return HttpResponse("ready", status=200)
 
 
 @api_view(['POST'])
